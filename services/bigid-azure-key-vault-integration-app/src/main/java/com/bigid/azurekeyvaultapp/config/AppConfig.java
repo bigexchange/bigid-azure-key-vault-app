4b961fbbb38bdfb49fdbfa2fc8e9c92f85344614
Stanislav Naida <snaida@bigid.com>
2024-11-18 17:55:47 +0200

    BDT-101095: exclude empty properties from serialization

--- services/bigid-azure-key-vault-integration-app/src/main/java/com/bigid/azurekeyvaultapp/config/AppConfig.java
+++ services/bigid-azure-key-vault-integration-app/src/main/java/com/bigid/azurekeyvaultapp/config/AppConfig.java
@@ -1,6 +1,7 @@
 package com.bigid.azurekeyvaultapp.config;
 
 import com.bigid.azurekeyvaultapp.service.ExecutionService;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -25,6 +26,7 @@ public class AppConfig {
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
         return objectMapper;
     }
 }
